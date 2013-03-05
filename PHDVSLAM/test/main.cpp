dc84786297933479beb3e7d02d93e2effb5331be
Jose Franco <j.l.franco@me.com>
2013-03-05 17:00:44 +0000

    Moved structure around, added CMakeLists

---
+++ PHDVSLAM/test/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  test
+//
+//  Created by Jose Franco on 3/5/13.
+//  Copyright (c) 2013 Jose Franco. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+  // insert code here...
+  std::cout << "Hello, World!\n";
+    return 0;
+}
+
